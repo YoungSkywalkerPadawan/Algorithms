71f1ca7c23ba8e0666a025212bd39b442b46db60
Kai <2504391561@qq.com>
2024-03-31 20:45:55 +0800

    problem description

--- AlgorithmsCabin/Math/Distance/Problems.py
+++ AlgorithmsCabin/Math/Distance/Problems.py
@@ -25,6 +25,9 @@ from math import inf
 
 
 # lc391T4
+# 给你一个下标从 0 开始的数组 points,它表示二维平面上一些点的整数坐标，其中 points[i] = [xi, yi] 。
+# 两点之间的距离定义为它们的曼哈顿距离。
+# 请你恰好移除一个点，返回移除后任意两点之间的最大距离可能的最小值。
 def minimumDistance(points: List[List[int]]) -> int:
     xs = SortedList()
     ys = SortedList()
