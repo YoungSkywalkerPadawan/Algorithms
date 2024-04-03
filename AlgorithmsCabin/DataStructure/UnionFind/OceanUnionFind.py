00af769691bd6115f77b88921659798d0c53841b
Kai <2504391561@qq.com>
2024-04-03 19:21:21 +0800

    Union Find problems

---
+++ AlgorithmsCabin/DataStructure/UnionFind/OceanUnionFind.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class OceanUnionFind:
+    def __init__(self, grid: List[List[int]]):
+        self.m = len(grid)
+        self.n = len(grid[0])
+        size = self.m * self.n
+        self.fa = list(range(size))
+        # 当前连通的val值
+        self.val = [0] * size
+        # 当前连通能达到的边界情况
+        self.border = [[0] * 4 for _ in range(size)]
+        for i, row in enumerate(grid):
+            for j, x in enumerate(row):
+                self.val[i * self.n + j] = x
+                self.border[i * self.n + j] = [j, i, j, i]
+
+    def find(self, x: int) -> int:
+        if self.fa[x] != x:
+            self.fa[x] = self.find(self.fa[x])
+        return self.fa[x]
+
+    def unite(self, x: int, y: int) -> None:
+        fx = self.find(x)
+        fy = self.find(y)
+        if self.val[fy] > self.val[fx] or fy == fx:
+            return
+        l1, u1, r1, d1 = self.border[fx]
+        l2, u2, r2, d2 = self.border[fy]
+        self.border[fx] = [min(l1, l2), min(u1, u2), max(r1, r2), max(d1, d2)]
+        if self.val[fy] == self.val[fx]:
+            self.fa[fy] = fx
+
+    def check(self, x: int) -> bool:
+        fx = self.find(x)
+        l, u, r, d = self.border[fx]
+        return (l == 0 or u == 0) and (r == self.n - 1 or d == self.m - 1)
---
+++ AlgorithmsCabin/DataStructure/UnionFind/ProblemSet/problems.py
@@ -0,0 +1,34 @@
+from heapq import heappush, heappop
+from typing import List
+from AlgorithmsCabin.DataStructure.UnionFind.OceanUnionFind import OceanUnionFind
+
+
+# lc417 太平洋大西洋水流问题
+def pacificAtlantic(heights: List[List[int]]) -> List[List[int]]:
+    # 并查集
+    # 从小到大构建连通块(fa记录当前连通块能达到最左，上，右，下的位置)
+    # 用堆维护大小
+    h = []
+    m = len(heights)
+    n = len(heights[0])
+    for i, row in enumerate(heights):
+        for j, x in enumerate(row):
+            heappush(h, (x, i, j))
+    # 并查集 初始化
+    UF = OceanUnionFind(heights)
+
+    while h:
+        val_x, x1, y1 = heappop(h)
+        x = x1 * n + y1
+        for x2, y2 in (x1 + 1, y1), (x1 - 1, y1), (x1, y1 + 1), (x1, y1 - 1):
+            if 0 <= x2 < m and 0 <= y2 < n:
+                y = x2 * n + y2
+                # 对该点上下左右方向进行连通操作
+                UF.unite(x, y)
+    ans = []
+    # 完成连通图构建，开始对每个点进行确认
+    for i in range(m):
+        for j in range(n):
+            if UF.check(i * n + j):
+                ans.append([i, j])
+    return ans
