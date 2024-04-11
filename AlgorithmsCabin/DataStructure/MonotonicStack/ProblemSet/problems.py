e013fa93e593d9380ede4dd8d0ca82793c04633c
Kai <2504391561@qq.com>
2024-04-11 19:57:15 +0800

    monotonic stack

--- AlgorithmsCabin/DataStructure/MonotonicStack/ProblemSet/problems.py
+++ AlgorithmsCabin/DataStructure/MonotonicStack/ProblemSet/problems.py
@@ -1,4 +1,4 @@
-from collections import deque
+from collections import deque, Counter
 from itertools import accumulate
 from typing import List
 
@@ -40,3 +40,29 @@ def totalStrength(strength: List[int]) -> int:
     for i, x in enumerate(strength):
         ans += x * ((i - left[i]) * (ss[right[i] + 1] - ss[i + 1]) - (right[i] - i) * (ss[i + 1] - ss[left[i] + 1]))
     return ans % MOD
+
+
+# lc2030 含特定字母的最小子序列
+def smallestSubsequence(s: str, k: int, letter: str, repetition: int) -> str:
+    cnt = Counter(s)
+    dq = deque()
+    n = len(s)
+    # 在满足选择的数量大于等于k的情况下，且letter足够的情况下，栈顶的元素大于当前元素则弹出
+    for i, x in enumerate(s):
+        while n - i > k - len(dq) and dq and dq[-1] > x:
+            if dq[-1] == letter and cnt[letter] == repetition:
+                break
+            y = dq.pop()
+            cnt[y] -= 1
+        dq.append(x)
+    # 完成一次遍历，选择的元素可能大于k,需要弹出，若弹出的是letter,考虑当前letter是否足够，不够后续要补,因为要补，所以k额外减少
+    while len(dq) > k:
+        y = dq.pop()
+        cnt[y] -= 1
+        if y == letter and cnt[letter] < repetition:
+            k -= 1
+    # 补不够的letter
+    while cnt[letter] < repetition:
+        dq.append(letter)
+        cnt[letter] += 1
+    return "".join(dq)
