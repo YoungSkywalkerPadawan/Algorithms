9ca73fe6f49206ffb2b3f341f8a61cd6d08d207c
Kai <2504391561@qq.com>
2024-04-16 20:09:20 +0800

    State Compression

--- AlgorithmsCabin/DynamicProgramming/StateCompression/EnumerateSubsets.py
+++ AlgorithmsCabin/DynamicProgramming/StateCompression/EnumerateSubsets.py
@@ -121,3 +121,62 @@ def numberOfGoodSubsets(nums: List[int]) -> int:
                 fx[bit | j] = (fx[j] * cnt[i]) % MOD
             bit_sum[bit | j] = bit_sum[j] | bit_val[i]
     return (sum(fx) - 1 - sub) % MOD
+
+
+# LCP53 守护太空城
+def defendSpaceCity(time: List[int], position: List[int]) -> int:
+    n, m = max(position), 1 << max(time)
+    rain = [0] * (n + 1)
+    for t, p in zip(time, position):
+        rain[p] |= 1 << (t - 1)
+
+    union = [0] * m
+    single = [0] * m
+    for i in range(1, m):
+        lb = i & -i
+        j = i ^ lb
+        lb2 = j & -j
+        union[i] = union[j] + (1 if lb == (lb2 >> 1) else 3)  # lb == (lb2 >> 1) 表示两个时间点相邻
+        single[i] = single[j] + (1 if lb == (lb2 >> 1) else 2)  # 递推
+
+    @cache  # 缓存装饰器，避免重复计算 dfs 的结果
+    def dfs(x: int, y: int) -> int:
+        if x < 0:
+            return 0
+        ans = inf
+        pre = mask = (m - 1) ^ y
+        while True:  # 枚举 j 的补集 mask 中的子集 pre
+            cost = dfs(x - 1, pre) + union[y] + single[(mask ^ pre) & rain[x]]
+            ans = min(ans, cost)
+            if pre == 0:
+                break
+            pre = (pre - 1) & mask
+        return ans
+
+    return dfs(n, 0)
+
+
+# LCP04 覆盖
+def domino(n: int, m: int, broken: List[List[int]]) -> int:
+    a = [(1 << m) - 1] * n
+    for x, y in broken:
+        a[x] -= (1 << y)
+
+    @cache  # 缓存装饰器，避免重复计算 dfs 的结果
+    def dp(i: int, j: int) -> int:
+        if i < 0:
+            return 0
+
+        def dfs(cur_i, s):
+            if cur_i == m:
+                return dp(i - 1, s)
+            res = dfs(cur_i + 1, s)  # 放弃当前这个格子
+            if cur_i <= m - 2 and (1 << cur_i) & s > 0 and (1 << (cur_i + 1)) & s > 0:  # 横放一块骨牌
+                res = max(res, 1 + dfs(cur_i + 2, s ^ (1 << cur_i) ^ (1 << (cur_i + 1))))
+            if (1 << cur_i) & s > 0 and (1 << cur_i) & j > 0:  # 竖放一块骨牌，前提是上一行这一列有空位
+                res = max(res, 1 + dfs(cur_i + 1, s ^ (1 << cur_i)))
+            return res
+
+        return dfs(0, a[i])
+
+    return dp(n - 1, 0)
