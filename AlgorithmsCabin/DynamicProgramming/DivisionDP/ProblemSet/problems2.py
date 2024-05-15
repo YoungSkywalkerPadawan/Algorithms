5f2663822704b4ec30230ffb82a57aa1e5123fe6
Kai <2504391561@qq.com>
2024-05-15 23:02:36 +0800

    Division DP

--- AlgorithmsCabin/DynamicProgramming/DivisionDP/ProblemSet/problems2.py
+++ AlgorithmsCabin/DynamicProgramming/DivisionDP/ProblemSet/problems2.py
@@ -3,13 +3,13 @@
 # 一般定义f[i][j] 表示将长为j 的前缀a[:j] 分成i 个连续子数组所得到的最优解。
 # 枚举最后一个子数组的左端点 𝐿，从 f[i−1][L] 转移到 f[i][j]，并考虑 a[L:j] 对最优解的影响。
 from functools import cache
+from itertools import accumulate
 from math import inf
 from typing import List
 
 
 # lc3117 划分数组得到最小的值的和
 def minimumValueSum(nums: List[int], andValues: List[int]) -> int:
-
     @cache
     def dfs(x: int, y: int, pre: int) -> int:
         if y >= 0 and pre < andValues[y]:
@@ -27,3 +27,60 @@ def minimumValueSum(nums: List[int], andValues: List[int]) -> int:
     ans = dfs(len(nums) - 2, len(andValues) - 1, nums[-1]) + nums[-1]
     dfs.cache_clear()
     return ans if ans < inf else -1
+
+
+# lc1478 安排邮筒
+def minDistance(houses: List[int], k: int) -> int:
+    houses.sort()
+    n = len(houses)
+    pre = list(accumulate(houses, initial=0))
+
+    @cache
+    def dfs(x: int, y: int) -> int:
+        if x == 0:
+            return inf
+        if y == 1:
+            mid = x // 2
+            if x % 2:
+                return pre[x] - pre[mid + 1] - pre[mid]
+            else:
+                return pre[x] - pre[mid] - pre[mid]
+
+        res = inf
+        for i in range(x):
+            mid = i + (x - i) // 2
+            if (x - i) % 2:
+                cur = pre[x] - pre[mid + 1] - (pre[mid] - pre[i])
+            else:
+                cur = pre[x] - pre[mid] - (pre[mid] - pre[i])
+            res = min(res, dfs(i, y - 1) + cur)
+        return res
+
+    ans = dfs(n, k)
+    dfs.cache_clear()
+    return ans
+
+
+# lc1278 分割回文串III
+def palindromePartition(s: str, k: int) -> int:
+    dp = [[0] * len(s) for _ in s]
+    for i in range(len(s) - 2, -1, -1):
+        dp[i][i + 1] = 0 if s[i] == s[i + 1] else 1
+        for j in range(i + 2, len(s)):
+            dp[i][j] = dp[i + 1][j - 1] if s[i] == s[j] else dp[i + 1][j - 1] + 1
+    n = len(s)
+
+    @cache
+    def dfs(x: int, y: int) -> int:
+        if x == 0:
+            return inf
+        if y == 1:
+            return dp[0][x - 1]
+        res = inf
+        for i_ in range(x):
+            res = min(res, dfs(i_, y - 1) + dp[i_][x - 1])
+        return res
+
+    ans = dfs(n, k)
+    dfs.cache_clear()
+    return ans
