e55e40d6814c7bd2b1a69a47762eec3af3931376
Kai <2504391561@qq.com>
2024-07-06 14:57:09 +0800

    DP

--- AlgorithmsCabin/DynamicProgramming/Others/problems.py
+++ AlgorithmsCabin/DynamicProgramming/Others/problems.py
@@ -48,3 +48,46 @@ def cf1989E():
             dp[i][j] = d
     print(dp[-1][-1])
     return
+
+
+def cf1987F():
+    n = int(input())
+    a = list(map(int, input().split()))
+    inf = 10 ** 9
+    # dp[l][r]移除子段al...ar而需要在l左边进行的最小操作数
+    dp = [[inf] * (n + 2) for _ in range(n + 2)]
+    for i in range(1, n + 2):
+        dp[i][i-1] = 0
+
+    # 被移除必须 必须 i >= ai 且(i-ai)%2 == 0,下标从1开始
+    for k in range(1, n):
+        for l in range(1, n - k + 1):
+            # 不可能被移除
+            if a[l-1] > l:
+                continue
+            if (l - a[l-1]) % 2 != 0:
+                continue
+
+            # a[l] 被移除首先前面必须要操作的次数
+            v = (l - a[l-1]) // 2
+            r = l + k
+            # 看 al, am能否操作,即dp[l+1][m-1] <= v否则,a[l]能被移除的时候,al,am之间还有元素
+            for m in range(l + 1, r + 1, 2):
+                if dp[l + 1][m-1] <= v:
+                    # dp[l][m]能被移除了,操作次数为（m-l+1)//2
+                    new_val = max(v, dp[m + 1][r] - (m - l + 1) // 2)
+                    dp[l][r] = min(dp[l][r], new_val)
+
+    # 对前缀a1,,,an执行操作的最大次数
+    dp2 = [0] * (n + 1)
+
+    for i in range(1, n+1):
+        dp2[i] = dp2[i-1]
+
+        for j in range(1, i):
+            # [j, i]这一段可以j前面操作后被移除,操作次数为(i-j+1)//2
+            if dp[j][i] <= dp2[j-1]:
+                dp2[i] = max(dp2[i], dp2[j-1] + (i - j + 1) // 2)
+
+    print(dp2[-1])
+    return
