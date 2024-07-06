da06db8543c5a40b6e44cf2fbcecf434e5b3a631
Kai <2504391561@qq.com>
2024-07-06 14:51:13 +0800

    TreeShape DP

--- AlgorithmsCabin/DynamicProgramming/Others/problems.py
+++ AlgorithmsCabin/DynamicProgramming/Others/problems.py
@@ -1,3 +1,6 @@
+MOD = 998244353
+
+
 def cf1989D():
     # n, m = map(int, input().split())
     a = list(map(int, input().split()))
@@ -23,4 +26,25 @@ def cf1989D():
         else:
             res += dp[x]
     print(res)
+    return
+
+
+def cf1989E():
+    n, k = map(int, input().split())
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    dp[0][0] = 1
+    sm = [0] * (k + 1)
+    # sm[0] = 1
+    # dp[i][j] = dp[i-1, j-1] +... + dp[0,j-1]
+    for i in range(1, n + 1):
+        for j in range(k + 1):
+            if i >= 3:
+                sm[j] += dp[i - 3][j]
+                sm[j] %= MOD
+            d = sm[max(j - 1, 0)] + dp[i - 1][max(j - 1, 0)]
+            if i == n or i == 2:
+                d += dp[i - 2][max(j - 1, 0)]
+            d %= MOD
+            dp[i][j] = d
+    print(dp[-1][-1])
     return
--- AlgorithmsCabin/DynamicProgramming/TreeShaped/ProblemSet/problems2.py
+++ AlgorithmsCabin/DynamicProgramming/TreeShaped/ProblemSet/problems2.py
@@ -1,3 +1,5 @@
+from collections import deque
+from math import inf
 from types import GeneratorType
 
 
@@ -75,5 +77,57 @@ def cf633F():
         yield maxChain, max(maxPathV, maxPathW), maxChainPath
 
     dfs(0, -1)
+    print(ans)
+    return
+
+
+def cf1987E():
+    n = int(input())
+    a = list(map(int, input().split()))
+    p = list(map(int, input().split()))
+    # 建树
+    g = [[] for _ in range(n)]
+    d = [0] * n
+    for i, x_ in enumerate(p):
+        i += 1
+        x_ -= 1
+        g[x_].append(i)
+        d[i] = d[x_] + 1
+
+    # 树形DP 每个节点维护当前最小分支深度和 已经操作的次数
+    # 计算当前节点，根据其子节点的值， 确定是否要操作，如果要操作，根据子节点中最小分支深度确定操作次数
+    # 操作总次数为子节点的操作次数之和加上这一次的操作次数
+    # 返回新的最小分支深度和已经操作次数
+    ops = [0] * n
+    extra = [0] * n
+
+    @bootstrap
+    def dfs(x: int) -> None:
+        cur_op = 0
+        if not g[x]:
+            extra[x] = inf
+        else:
+            extra[x] = -a[x]
+            for y in g[x]:
+                yield dfs(y)
+                op = ops[y]
+                extra[x] += a[y]
+                cur_op += op
+            q = deque([x])
+            while q and extra[x] < 0:
+                v = q.popleft()
+                for u in g[v]:
+                    delta = min(-extra[x], extra[u])
+                    if delta > 0:
+                        extra[x] += delta
+                        extra[u] -= delta
+                        cur_op += delta * (d[u] - d[x])
+                    q.append(u)
+
+        ops[x] = cur_op
+        yield
+
+    dfs(0)
+    ans = ops[0]
     print(ans)
     return
