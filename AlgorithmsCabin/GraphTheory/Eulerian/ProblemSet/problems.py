f34a433abee48acf773e16abcf0a61f5a1dc29de
Kai <2504391561@qq.com>
2024-07-21 13:24:29 +0800

    Eulerian

--- AlgorithmsCabin/GraphTheory/Eulerian/ProblemSet/problems.py
+++ AlgorithmsCabin/GraphTheory/Eulerian/ProblemSet/problems.py
@@ -9,7 +9,7 @@
 # 判别法
 # 1.无向图是欧拉图当且仅当：非零度顶点是连通的;顶点的度数都是偶数
 # 2.无向图是半欧拉图当且仅当：非零度顶点是连通的;恰有 2 个奇度顶点
-# 3.有向图是半欧拉图当且仅当：非零度顶点是连通的;每个顶点的入度和出度相等
+# 3.有向图是欧拉图当且仅当：非零度顶点是连通的;每个顶点的入度和出度相等
 # 4.有向图是半欧拉图当且仅当：非零度顶点是弱连通的;至多一个顶点的出度与入度之差为 1；至多一个顶点的入度与出度之差为 1；其他顶点的入度和出度相等
 
 # Hierholzer 算法 用于在连通图中寻找欧拉路径
@@ -19,8 +19,10 @@
 from collections import defaultdict, Counter
 from typing import List
 
-
 # lc2097 合法重新排列数对
+from AlgorithmsCabin.Math.Util.utils import mint
+
+
 def validArrangement(pairs: List[List[int]]) -> List[List[int]]:
     # Kruskal算法出度 - 入度大于0的点存在，则该点为起点，反之随便哪个点作为起点。
     g = defaultdict(list)
@@ -45,3 +47,85 @@ def validArrangement(pairs: List[List[int]]) -> List[List[int]]:
             break
     dfs(node)
     return ans[::-1]
+
+
+def cf1994F():
+    n, m = mint()
+    g = [[] for _ in range(n)]
+    u = [0] * m
+    v = [0] * m
+    c = [0] * m
+    deg = [0] * n
+    for i in range(m):
+        u[i], v[i], c[i] = mint()
+        u[i] -= 1
+        v[i] -= 1
+        deg[u[i]] ^= c[i]
+        deg[v[i]] ^= c[i]
+        g[u[i]].append(i)
+        g[v[i]].append(i)
+
+    vis = [0] * n
+
+    def dfs(cur_x: int) -> None:
+        vis[cur_x] = 1
+        for cur_idx in g[cur_x]:
+            cur_y = u[cur_idx] ^ v[cur_idx] ^ cur_x
+            if vis[cur_y] or c[cur_idx] == 1:
+                continue
+
+            dfs(cur_y)
+            if deg[cur_y] == 1:
+                deg[cur_x] ^= 1
+                deg[cur_y] ^= 1
+                c[cur_idx] = 1
+
+    for i in range(n):
+        if vis[i] == 1:
+            continue
+        dfs(i)
+        if deg[i] == 1:
+            print("NO")
+            return
+    print("YES")
+    # 开始找欧拉回路
+    ans = []
+    #
+    # @bootstrap
+    # def find(x: int) -> None:
+    #     while g[x]:
+    #         idx = g[x].pop()
+    #         y = u[idx] ^ v[idx] ^ x
+    #         if c[idx] == 0:
+    #             continue
+    #
+    #         c[idx] = 0
+    #         yield find(y)
+    #
+    #     ans.append(x+1)
+    #     yield
+    # find(0)
+    stk = [0]
+    while stk:
+        x = stk.pop()
+        if not g[x]:
+            ans.append(x + 1)
+        else:
+            f = False
+            while g[x]:
+                idx = g[x].pop()
+                y = u[idx] ^ v[idx] ^ x
+                if c[idx] == 0:
+                    continue
+                c[idx] = 0
+                f = True
+                stk.append(x)
+                stk.append(y)
+                break
+            if not f:
+                ans.append(x + 1)
+
+    print(len(ans) - 1)
+    print(*ans)
+
+    return
