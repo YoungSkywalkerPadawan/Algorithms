2d7f3fc67e82963669b5644f7868371732d8bdf6
Kai <2504391561@qq.com>
2024-08-17 20:54:48 +0800

    bit masks

--- AlgorithmsCabin/DynamicProgramming/StateCompression/problems2.py
+++ AlgorithmsCabin/DynamicProgramming/StateCompression/problems2.py
@@ -1,4 +1,4 @@
-from AlgorithmsCabin.Math.Util.utils import mint
+from AlgorithmsCabin.Math.Util.utils import mint, sint
 
 
 def cf1995D():
@@ -21,7 +21,7 @@ def cf1995D():
     dp[tot ^ mask] = 1
     for i in range(k, n):
         cnt[ord(s[i]) - ord('A')] += 1
-        cnt[ord(s[i-k]) - ord('A')] -= 1
+        cnt[ord(s[i - k]) - ord('A')] -= 1
         mask = 0
         for j in range(c):
             if cnt[j]:
@@ -38,4 +38,38 @@ def cf1995D():
         if dp[i] == 0 and (i >> (ord(s[-1]) - ord('A'))) & 1:
             ans = min(ans, i.bit_count())
     print(ans)
+    return
+
+
+def cf1935E():
+    n = sint()
+    cnt_common = [[0] * 30 for _ in range(n + 1)]
+    cnt_mx = [[0] * 30 for _ in range(n + 1)]
+    for i in range(n):
+        x, y = mint()
+        mask = (1 << (x ^ y).bit_length()) - 1
+        # 除了公共前缀最大能到多少
+        mx = y & mask
+        # 公共前缀
+        pre = y - mx
+        for bit in range(30):
+            cnt_common[i + 1][bit] = cnt_common[i][bit] + ((pre >> bit) & 1)
+            cnt_mx[i + 1][bit] = cnt_mx[i][bit] + ((mx >> bit) & 1)
+
+    ans = []
+    for _ in range(sint()):
+        l, r = mint()
+        l -= 1
+        x = 0
+        for bit in range(29, -1, -1):
+            if cnt_common[r][bit] - cnt_common[l][bit] or cnt_mx[r][bit] - cnt_mx[l][bit]:
+                x |= 1 << bit
+                if cnt_mx[r][bit] - cnt_mx[l][bit] > 1:
+                    x |= (1 << bit) - 1
+                    break
+                if cnt_mx[r][bit] - cnt_mx[l][bit] > 0 and cnt_common[r][bit] - cnt_common[l][bit] > 0:
+                    x |= (1 << bit) - 1
+                    break
+        ans.append(x)
+    print(*ans)
     return
