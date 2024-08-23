35db7132d1d8fb94f216fc55dabac6d2e2fac407
Kai <2504391561@qq.com>
2024-08-23 21:03:11 +0800

    DP

--- AlgorithmsCabin/DynamicProgramming/LinearDP/ProblemSet/problems.py
+++ AlgorithmsCabin/DynamicProgramming/LinearDP/ProblemSet/problems.py
@@ -1,5 +1,6 @@
 # 发生在前缀/后缀之间的转移，例如从 f[i−1]转移到f[i]，或者从f[j]转移到f[i]
 from functools import cache
+from heapq import heappush, heappop
 from math import inf
 from typing import List
 
@@ -45,3 +46,50 @@ def minimumFinishTime(tires: List[List[int]], changeTime: int, numLaps: int) -> 
         return changeTime + res
 
     return dfs(numLaps)
+
+
+def cf1932F():
+    n, m = map(int, input().split())
+    # 先记录每个位置的下一个位置
+    res = []
+    mx = 0
+    for _ in range(m):
+        x, y = map(int, input().split())
+        res.append((x, y))
+        if y > mx:
+            mx = y
+    res.sort()
+    nxt = [0] * (mx + 1)
+    siz = [0] * (mx + 1)
+    reach = 0
+    h = []
+    # 最小堆，统计当前位置有几个
+    l = 0
+    for i in range(mx + 1):
+        while l < m and res[l][0] == i:
+            reach = max(reach, res[l][1])
+            heappush(h, res[l][1])
+            l += 1
+        # 出堆
+        while h and h[0] < i:
+            heappop(h)
+
+        nxt[i] = max(reach + 1, i + 1)
+        siz[i] = len(h)
+
+    # 开始dp 选或不选
+    # def dfs(x: int) -> int:
+    #     if x > mx :
+    #         return  0
+    #
+    #     res = dfs(x+1)
+    #     res2 = siz[x] + dfs(nxt[x])
+    #     return max(res, res2)
+    #
+    # ans = dfs(0)
+    dp = [0] * (mx + 2)
+    for i in range(mx, -1, -1):
+        dp[i] = max(dp[i + 1], siz[i] + dp[nxt[i]])
+
+    print(dp[0])
+    return
