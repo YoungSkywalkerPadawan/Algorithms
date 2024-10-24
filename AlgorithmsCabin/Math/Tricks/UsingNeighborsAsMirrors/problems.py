dd779f7815d10619bc7782e7c0bc3820399dcdcd
Kai <2504391561@qq.com>
2024-10-24 20:24:04 +0800

    using neighbor as mirrors

--- AlgorithmsCabin/Math/Tricks/UsingNeighborsAsMirrors/problems.py
+++ AlgorithmsCabin/Math/Tricks/UsingNeighborsAsMirrors/problems.py
@@ -1,4 +1,4 @@
-from AlgorithmsCabin.Math.Util.utils import sint, ints
+from AlgorithmsCabin.Math.Util.utils import sint, ints, mint
 
 
 def cf1995C():
@@ -7,19 +7,19 @@ def cf1995C():
     cnt = 0
     ans = 0
     for i in range(1, n):
-        if a[i] == a[i-1]:
+        if a[i] == a[i - 1]:
             ans += cnt
-        elif a[i] < a[i-1]:
+        elif a[i] < a[i - 1]:
             if a[i] == 1:
                 print(-1)
                 return
             cur = a[i]
-            while cur < a[i-1]:
+            while cur < a[i - 1]:
                 cur = cur * cur
                 cnt += 1
             ans += cnt
         else:
-            cur = a[i-1]
+            cur = a[i - 1]
             if cnt > 0:
                 while cur <= a[i] and cnt > 0:
                     cnt -= 1
@@ -52,7 +52,42 @@ def cf1954E():
         res = 0
         for i in range(1, mx + 1, k):
             # 计算[i,i+k-1]的这些数需要的总时间
-            res += ((i - 1) // k + 1) * (cnt[min(i + k - 1, mx)] - cnt[i-1])
+            res += ((i - 1) // k + 1) * (cnt[min(i + k - 1, mx)] - cnt[i - 1])
         ans.append(res)
     print(*ans)
+    return
+
+
+def cf2030D():
+    n, q = mint()
+    a = ints()
+    s = list(input())
+    pre = [0] * n
+    pre[0] = a[0]
+    bad = [0] * n
+    for i in range(1, n):
+        pre[i] = max(pre[i - 1], a[i])
+        if pre[i - 1] != i:
+            bad[i] = 1
+
+    cnt = 0
+    for i in range(1, n):
+        if bad[i] == 1 and "".join(s[i - 1:i + 1]) == "LR":
+            cnt += 1
+
+    for _ in range(q):
+        x = sint()
+        x -= 1
+        if bad[x] == 1 and "".join(s[x - 1:x + 1]) == "LR":
+            cnt -= 1
+        if bad[x + 1] == 1 and "".join(s[x:x + 2]) == "LR":
+            cnt -= 1
+
+        s[x] = 'L' if s[x] == 'R' else 'R'
+        if bad[x] == 1 and "".join(s[x - 1:x + 1]) == "LR":
+            cnt += 1
+        if bad[x + 1] == 1 and "".join(s[x:x + 2]) == "LR":
+            cnt += 1
+        print("YES" if cnt == 0 else "NO")
+
     return
