283726943deda2f3ecd463141c8aab34594226fe
Kai <2504391561@qq.com>
2024-12-02 21:52:16 +0800

    z function

--- AlgorithmsCabin/BasicAlgorithm/String/zfunction.py
+++ AlgorithmsCabin/BasicAlgorithm/String/zfunction.py
@@ -127,3 +127,22 @@ def cf432D():
     for x in res:
         print(*x)
     return
+
+
+def cf2010C():
+    s = input()
+    n = len(s)
+    z = [0] * n
+    l = r = 0
+    for i in range(1, (n + 1) // 2):
+        if i <= r:
+            z[i] = min(z[i - l], r - i + 1)  # z函数的核心思想
+        while i + z[i] < n and s[z[i]] == s[i + z[i]]:
+            l, r = i, i + z[i]
+            z[i] += 1
+        if i + z[i] >= n:
+            print("YES")
+            print(s[i:])
+            return
+    print("NO")
+    return
