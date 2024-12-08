56198016128f56b970904a1faccea7508d0da1e7
Kai <2504391561@qq.com>
2024-12-08 21:34:45 +0800

    greedy

--- AlgorithmsCabin/BasicAlgorithm/Greedy/problems.py
+++ AlgorithmsCabin/BasicAlgorithm/Greedy/problems.py
@@ -1,5 +1,7 @@
 from bisect import bisect_right
 
+from AlgorithmsCabin.Math.Util.utils import mint
+
 
 def cf582B():
     n, t = map(int, input().split())
@@ -19,4 +21,26 @@ def cf582B():
         if t == 0:
             break
     print(len(f) + t * d)
+    return
+
+
+def cf2042C():
+    n, k = mint()
+    s = input()
+    suf = [0] * (n - 1)
+    suf[-1] = -1 if s[-1] == '0' else 1
+    for i in range(n - 2, 0, -1):
+        if s[i] == '0':
+            suf[i-1] = suf[i] - 1
+        else:
+            suf[i-1] = suf[i] + 1
+    c = 0
+    ans = 1
+    suf.sort(reverse=True)
+    for x in suf:
+        c += x
+        ans += 1
+        if c >= k:
+            break
+    print(ans if c >= k else -1)
     return
