c9352a2f5b3ca6c03d0c20ea78230eaf7ea7040d
Kai <2504391561@qq.com>
2024-12-21 22:35:22 +0800

    bit

--- AlgorithmsCabin/DataStructure/BinaryIndexedTree/ProblemSet/problems3.py
+++ AlgorithmsCabin/DataStructure/BinaryIndexedTree/ProblemSet/problems3.py
@@ -2,7 +2,7 @@ from bisect import bisect_right, bisect_left
 from collections import defaultdict
 
 from AlgorithmsCabin.DataStructure.BinaryIndexedTree.BIT2 import BIT
-from AlgorithmsCabin.Math.Util.utils import mint, ints
+from AlgorithmsCabin.Math.Util.utils import mint, ints, ints2
 
 
 def cf383C():
@@ -82,4 +82,74 @@ def cf1045G():
         tree = bit[q]
         tree.add(idx, 1)
     print(ans)
+    return
+
+
+def cf1899G():
+    n, q = mint()
+    g = [[] for _ in range(n)]
+    for _ in range(n - 1):
+        u, v = mint()
+        u -= 1
+        v -= 1
+        g[u].append(v)
+        g[v].append(u)
+
+    p = ints2()
+    pos = [-1] * n
+
+    for i, x in enumerate(p):
+        pos[x] = i
+
+    # 离线询问
+    ql = []
+    qr = []
+    queries = [[] for _ in range(n)]
+    for i in range(q):
+        l, r, x = ints2()
+        ql.append(l)
+        qr.append(r)
+        queries[x].append(i)
+
+    # dfs序
+    dq = [0]
+    order = []
+    ls = [-1] * n
+    rs = [-1] * n
+    ts = 0
+
+    while dq:
+        u = dq.pop()
+        if u >= 0:
+            order.append(u)
+            dq.append(~u)
+            ls[u] = ts
+            ts += 1
+
+            for v in g[u]:
+                if ls[v] == -1:
+                    dq.append(v)
+        else:
+            rs[~u] = ts
+
+    # 保存
+    tmp = [[] for _ in range(n + 1)]
+    for i in range(n):
+        for idx in queries[i]:
+            tmp[ls[i]].append(~idx)
+            tmp[rs[i]].append(idx)
+
+    ans = [0] * q
+    bit = BIT(n)
+    for i in range(n):
+        bit.add(pos[order[i]], 1)
+        for x in tmp[i+1]:
+            if x >= 0:
+                ans[x] += bit.range_sm(ql[x], qr[x] + 1)
+            else:
+                x = ~x
+                ans[x] -= bit.range_sm(ql[x], qr[x] + 1)
+    for x in ans:
+        print("YES" if x else "NO")
+    print()
     return
