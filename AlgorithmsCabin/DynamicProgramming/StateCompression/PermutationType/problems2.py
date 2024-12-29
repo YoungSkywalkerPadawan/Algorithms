e2b0563b2c82da6550038659b3e7a73d87d333a1
Kai <2504391561@qq.com>
2024-12-29 10:26:06 +0800

    dp

--- AlgorithmsCabin/DynamicProgramming/StateCompression/PermutationType/problems2.py
+++ AlgorithmsCabin/DynamicProgramming/StateCompression/PermutationType/problems2.py
@@ -1,3 +1,5 @@
+from math import inf
+
 from AlgorithmsCabin.Math.Util.utils import sint
 
 
@@ -31,4 +33,71 @@ def cf1950G():
                 dp[mask | (1 << j)][j] = 1
     print(n - ans)
 
+    return
+
+
+def cf2051G():
+    n, q = [int(x) for x in input().split()]
+    # def getDis(s: int, t: int) -> int:
+    #     sm, mn = 0, 0
+    #     for i in range(q):
+    #         if idx[i] == t:
+    #             sm += int(ch[i] < 0)
+    #         if idx[i] == s:
+    #             sm -= int(ch[i] > 0)
+    #         mn = min(mn, sm)
+    #     return -mn + 1
+
+    # 相邻两条蛇的最小距离
+    mnDis = [[0] * n for _ in range(n)]
+    # 每条蛇最大长度
+    ll = [0] * n
+    rr = [0] * n
+    for _ in range(q):
+        u, v = input().split()
+        u = int(u) - 1
+        if v == '+':
+            ll[u] += 1
+        else:
+            rr[u] += 1
+        for i in range(n):
+            if u == i:
+                continue
+            mnDis[i][u] = max(mnDis[i][u], ll[u] - rr[i])
+
+    dp = [inf for _ in range((1 << n) * n)]
+    for i in range(n):
+        dp[n * (1 << i) + i] = 0
+    ctz = [0] * (1 << n)
+    for i in range(1 << n):
+        ctz[i] = (i & -i).bit_length() - 1
+    for mask in range(1, 1 << n):
+        cur = mask
+        while cur:
+            i = ctz[cur]
+            sub = (1 << n) - 1 - mask
+            cur &= cur - 1
+            pre = sub
+            while pre:
+                j = ctz[pre]
+                dp[n * (mask | 1 << j) + j] = min(dp[n * (mask | 1 << j) + j], dp[n * mask + i] + mnDis[j][i])
+                pre &= pre - 1
+    # dp = [inf] * (n * (1 << n))
+    # for i in range(n):
+    #     dp[(1 << i) + (i << n)] = 0
+    #
+    # for msk in range(1 << n):
+    #     for i in range(n):
+    #         if (msk >> i) & 1 == 0:
+    #             continue
+    #
+    #         for j in range(n):
+    #             new_mask = msk | (1 << j) | (j << n)
+    #             if dp[msk + (i << n)] + mnDis[i][j] < dp[new_mask]:
+    #                 dp[new_mask] = dp[msk + (i << n)] + mnDis[i][j]
+
+    ans = inf
+    for i in range(n):
+        ans = min(ans, dp[n*((1 << n) - 1)+i] + ll[i] + n)
+    print(ans)
     return
