9c8f0a48b48f73ca29d62770003d09b23769d879
Kai <2504391561@qq.com>
2025-01-11 19:42:36 +0800

    front and rear affix decomposition

--- AlgorithmsCabin/DataStructure/UnionFind/ProblemSet/problems2.py
+++ AlgorithmsCabin/DataStructure/UnionFind/ProblemSet/problems2.py
@@ -31,3 +31,32 @@ def cf1213G():
     print(*ans)
 
     return
+
+
+def cf1095F():
+    n, m = mint()
+    a = ints()
+    mn = min(a)
+    mn_idx = a.index(mn)
+    edges = []
+    for i, x in enumerate(a):
+        if i != mn_idx:
+            edges.append((mn + x, mn_idx, i))
+
+    for _ in range(m):
+        u, v, w = mint()
+        u -= 1
+        v -= 1
+        edges.append((w, u, v))
+
+    ans = 0
+    edges.sort()
+    uf = UnionFind(n)
+    for w, u, v in edges:
+        if uf.union(u, v):
+            ans += w
+            if uf.part == 1:
+                break
+    print(ans)
+
+    return
